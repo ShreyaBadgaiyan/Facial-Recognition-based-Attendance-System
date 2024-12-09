b6c4e282edcbbfe559d0ecff967dc92c4bad1b5d
Shreya Badgaiyan <117737111+ShreyaBadgaiyan@users.noreply.github.com>
2024-12-09 18:13:04 +0530

    edits done

--- attendance.py
+++ attendance.py
@@ -8,12 +8,26 @@ from time import strftime
 from datetime import datetime
 import os
 import numpy as np
+import csv
+from tkinter import filedialog
 
+myData=[]
 class Attendance:
     def __init__(self,root):
         self.root=root
         self.root.geometry("1530x790+0+0")
         self.root.title("Face Recognition System")
+
+        #=========================variables=============================
+
+        self.var_atten_id=StringVar()
+        self.var_atten_roll=StringVar()
+        self.var_atten_name=StringVar()
+        self.var_atten_dep=StringVar()
+        self.var_atten_time=StringVar()
+        self.var_atten_date=StringVar()
+        self.var_atten_attendance=StringVar()
+
 
 
         #first image
@@ -68,28 +82,28 @@ class Attendance:
         attendanceId_label=Label(left_inside_frame,text="AttendanceId:",font=("times new roman",13,"bold"),bg="white")
         attendanceId_label.grid(row=0,column=0,padx=10,pady=5,sticky=W)
          
-        attendanceID_entry=ttk.Entry(left_inside_frame,width=20,font=("times new roman",13,"bold"))
+        attendanceID_entry=ttk.Entry(left_inside_frame,width=20,textvariable=self.var_atten_id,font=("times new roman",13,"bold"))
         attendanceID_entry.grid(row=0,column=1,padx=10,pady=5,sticky=W)
 
         #Roll 
         rollLabel=Label(left_inside_frame,text="Roll:",font=("comicsansns",11,"bold"),bg="white")
         rollLabel.grid(row=0,column=2,padx=4,pady=8)
          
-        atten_roll=ttk.Entry(left_inside_frame,width=22,font=("comicsansns",11,"bold"))
+        atten_roll=ttk.Entry(left_inside_frame,width=22,textvariable=self.var_atten_roll,font=("comicsansns",11,"bold"))
         atten_roll.grid(row=0,column=3,pady=8)
 
         #Name 
         nameLabel=Label(left_inside_frame,text="Name:",font=("comicsansns",11,"bold"),bg="white")
         nameLabel.grid(row=1,column=0)
          
-        atten_name=ttk.Entry(left_inside_frame,width=22,font=("comicsansns",11,"bold"))
+        atten_name=ttk.Entry(left_inside_frame,width=22,textvariable=self.var_atten_name,font=("comicsansns",11,"bold"))
         atten_name.grid(row=1,column=1,pady=8)
 
         #Date 
         dateLabel=Label(left_inside_frame,text="Date:",font=("comicsansns",11,"bold"),bg="white")
         dateLabel.grid(row=1,column=0)
          
-        atten_date=ttk.Entry(left_inside_frame,width=22,font=("comicsansns",11,"bold"))
+        atten_date=ttk.Entry(left_inside_frame,width=22,textvariable=self.var_atten_date,font=("comicsansns",11,"bold"))
         atten_date.grid(row=1,column=1,pady=8)
 
         
@@ -98,21 +112,21 @@ class Attendance:
         depLabel=Label(left_inside_frame,text="Department:",font=("comicsansns",11,"bold"),bg="white")
         depLabel.grid(row=1,column=2)
          
-        atten_dep=ttk.Entry(left_inside_frame,width=22,font=("comicsansns",11,"bold"))
+        atten_dep=ttk.Entry(left_inside_frame,width=22,textvariable=self.var_atten_dep,font=("comicsansns",11,"bold"))
         atten_dep.grid(row=1,column=3,pady=8)
 
         #Time 
         timeLabel=Label(left_inside_frame,text="Time:",font=("comicsansns",11,"bold"),bg="white")
         timeLabel.grid(row=2,column=0)
          
-        atten_time=ttk.Entry(left_inside_frame,width=22,font=("comicsansns",11,"bold"))
+        atten_time=ttk.Entry(left_inside_frame,width=22,textvariable=self.var_atten_time,font=("comicsansns",11,"bold"))
         atten_time.grid(row=2,column=1,pady=8)
 
         #Date 
         dateLabel=Label(left_inside_frame,text="Date:",font=("comicsansns",11,"bold"),bg="white")
         dateLabel.grid(row=2,column=2)
          
-        atten_date=ttk.Entry(left_inside_frame,width=22,font=("comicsansns",11,"bold"))
+        atten_date=ttk.Entry(left_inside_frame,width=22,textvariable=self.var_atten_date,font=("comicsansns",11,"bold"))
         atten_date.grid(row=2,column=3,pady=8)
 
         #Attendance
@@ -120,7 +134,7 @@ class Attendance:
         attendanceLabel=Label(left_inside_frame,text="Attendance Status",font=("comicsansns",11,"bold"),bg="white")
         attendanceLabel.grid(row=3,column=0)
 
-        self.atten_status=ttk.Combobox(left_inside_frame,width=20,font="comicsansns 11 bold",state="readonly")
+        self.atten_status=ttk.Combobox(left_inside_frame,width=20,textvariable=self.var_atten_attendance,font="comicsansns 11 bold",state="readonly")
         self.atten_status["values"]=("Status","Present","Absent")
         self.atten_status.grid(row=3,column=1,pady=8)
         self.atten_status.current(0)
@@ -129,7 +143,7 @@ class Attendance:
         btn_frame=Frame(left_inside_frame,bd=2,relief=RIDGE,bg="white")
         btn_frame.place(x=0,y=300,width=715,height=35)
 
-        save_btn=Button(btn_frame,text="Import csv",width=17,font=("times new roman",13,"bold"),bg="blue",fg="white")
+        save_btn=Button(btn_frame,text="Import csv",command=self.importCsv,width=17,font=("times new roman",13,"bold"),bg="blue",fg="white")
         save_btn.grid(row=0,column=0)
 
        
@@ -139,7 +153,7 @@ class Attendance:
         delete_btn=Button(btn_frame,text="Update",width=17,font=("times new roman",13,"bold"),bg="blue",fg="white")
         delete_btn.grid(row=0,column=2)
 
-        reset_btn=Button(btn_frame,text="Reset",width=17,font=("times new roman",13,"bold"),bg="blue",fg="white")
+        reset_btn=Button(btn_frame,text="Reset",command=self.reset_data,width=17,font=("times new roman",13,"bold"),bg="blue",fg="white")
         reset_btn.grid(row=0,column=3)
          
 
@@ -185,10 +199,86 @@ class Attendance:
 
 
         self.AttendanceReportTable.pack(fill=BOTH,expand=1)
+
+        self.AttendanceReportTable.bind("<ButtonRelease>",self.get_cursor)
+
+#================================Fetch Data======================================
+
+    def fetchData(self,rows):
+         self.AttendanceReportTable.delete(*self.AttendanceReportTable.get_children())
+         for i in rows:
+             self.AttendanceReportTable.insert("",END,values=i) 
+     #import csv
+    def importCsv(self):
+         global myData
+         myData.clear()
+
+         fln = filedialog.askopenfilename(
+        initialdir=os.getcwd(),
+        title="Open CSV",
+        filetypes=(("CSV File", "*.csv"), ("All File", "*.*")),
+        parent=self.root
+    )
+         with open(fln) as myfile:
+              csvread=csv.reader(myfile,delimiter=",")
+              for i in csvread:
+                   myData.append(i)
+              self.fetchData(myData)
+
+       #export csv
+    def exportCsv(self):
+         try:
+              if len(myData)<1:
+                   messagebox.showerror("No Data","No Data found to export",parent=self.root)
+                   return False
+              fln = filedialog.asksaveasfilename(
+        initialdir=os.getcwd(),
+        title="Open CSV",
+        filetypes=(("CSV File", "*.csv"), ("All File", "*.*")),
+        parent=self.root
+    ) 
+              with open(fln,mode="w",newline="") as myfile:
+                   exp_write=csv.writer(myfile,delimiter=",")
+                   for i in myData:
+                        exp_write.writerow(i)
+                   messagebox.showinfo("Data Export","Your data exported to "+os.path.basename(fln)+" successfully")
+
+         except Exception as es:
+                   messagebox.showerror("Error",f"Due To:{str(es)}",parent=self.root)        
+
+    def get_cursor(self,event=""):
+         cursor_row=self.AttendanceReportTable.focus()
+         content=self.AttendanceReportTable.item(cursor_row)
+         rows=content['values']
+         self.var_atten_id.set(rows[0])
+         self.var_atten_roll.set(rows[1])
+         self.var_atten_name.set(rows[2])
+         self.var_atten_dep.set(rows[3])
+         self.var_atten_time.set(rows[4])
+         self.var_atten_date.set(rows[5])
+         self.var_atten_attendance.set(rows[6])
+
+
+    def reset_data(self):
+         self.var_atten_id.set("")
+         self.var_atten_roll.set("")
+         self.var_atten_name.set("")
+         self.var_atten_dep.set("")
+         self.var_atten_time.set("")
+         self.var_atten_date.set("")
+         self.var_atten_attendance.set("")
+
+              
+         
         
 
 
 if __name__=="__main__":
         root=Tk()
         obj=Attendance(root)
-        root.mainloop()   
+        root.mainloop()
+
+
+
+
+
--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 from tkinter import*
 from tkinter import ttk
+import tkinter.messagebox
 from PIL import Image,ImageTk
 from student import Student
+from time import strftime
+from datetime import datetime
 import os
+import tkinter
 from train import Train
 from face_recognise import Face_Recognition
-
+from attendance import Attendance
+from developer import Developer
+from help import Help
 
 class Face_Recognition_System:
     def __init__(self,root):
@@ -52,6 +58,17 @@ class Face_Recognition_System:
         title_lbl=Label(bgimg,text="FACIAL RECOGNITION ATTENDANCE SYSTEM SOFTWARE",font=("times new roman",35,"bold"),bg="white",fg="red")
         title_lbl.place(x=0,y=0,width=1530,height=45)
 
+
+        # ======================================time===================================
+
+        def time():
+            string=strftime('%H:%M:%S %p')
+            lbl.config(text=string)
+            lbl.after(1000,time)
+
+        lbl=Label(title_lbl,font=('times new roman',14,'bold'),background='white',foreground='blue')    
+        lbl.place(x=0,y=0,width=110,height=50)
+        time()
 #student button
 
         img4=Image.open(r"D:\Desktop\Computer Science\Facial Recognition based attendance system\collect_images\student.jpg")
@@ -82,10 +99,10 @@ class Face_Recognition_System:
         img6=img6.resize((220,220),Image.LANCZOS)
         self.photoimg6=ImageTk.PhotoImage(img6)
 
-        b1=Button(bgimg,image=self.photoimg6,cursor="hand2")  
+        b1=Button(bgimg,image=self.photoimg6,cursor="hand2",command=self.attendance_data)  
         b1.place(x=800,y=100,width=220,height=220)
 
-        b1=Button(bgimg,text="Attendance",cursor="hand2",font=("times new roman",15,"bold"),bg="darkblue",fg="white")  
+        b1=Button(bgimg,text="Attendance",cursor="hand2",command=self.attendance_data,font=("times new roman",15,"bold"),bg="darkblue",fg="white")  
         b1.place(x=800,y=300,width=220,height=40)
 
 #Help desk button
@@ -94,10 +111,10 @@ class Face_Recognition_System:
         img7=img7.resize((220,220),Image.LANCZOS)
         self.photoimg7=ImageTk.PhotoImage(img7)
 
-        b1=Button(bgimg,image=self.photoimg7,cursor="hand2")  
+        b1=Button(bgimg,image=self.photoimg7,cursor="hand2",command=self.help_data)  
         b1.place(x=1100,y=100,width=220,height=220)
 
-        b1=Button(bgimg,text="Help Desk",cursor="hand2",font=("times new roman",15,"bold"),bg="darkblue",fg="white")  
+        b1=Button(bgimg,text="Help Desk",cursor="hand2",command=self.help_data,font=("times new roman",15,"bold"),bg="darkblue",fg="white")  
         b1.place(x=1100,y=300,width=220,height=40)
 
         #Train button
@@ -130,10 +147,10 @@ class Face_Recognition_System:
         img10=img10.resize((220,220),Image.LANCZOS)
         self.photoimg10=ImageTk.PhotoImage(img10)
 
-        b1=Button(bgimg,image=self.photoimg10,cursor="hand2")  
+        b1=Button(bgimg,image=self.photoimg10,cursor="hand2",command=self.developer_data)  
         b1.place(x=800,y=380,width=220,height=220)
 
-        b1=Button(bgimg,text="Developer",cursor="hand2",font=("times new roman",15,"bold"),bg="darkblue",fg="white")  
+        b1=Button(bgimg,text="Developer",cursor="hand2",command=self.developer_data,font=("times new roman",15,"bold"),bg="darkblue",fg="white")  
         b1.place(x=800,y=580,width=220,height=40)
 
         #Exit button
@@ -142,15 +159,23 @@ class Face_Recognition_System:
         img11=img11.resize((220,220),Image.LANCZOS)
         self.photoimg11=ImageTk.PhotoImage(img11)
 
-        b1=Button(bgimg,image=self.photoimg11,cursor="hand2")  
+        b1=Button(bgimg,image=self.photoimg11,cursor="hand2",command=self.iExit)  
         b1.place(x=1100,y=380,width=220,height=220)
 
-        b1=Button(bgimg,text="Exit",cursor="hand2",font=("times new roman",15,"bold"),bg="darkblue",fg="white")  
+        b1=Button(bgimg,text="Exit",cursor="hand2",command=self.iExit,font=("times new roman",15,"bold"),bg="darkblue",fg="white")  
         b1.place(x=1100,y=580,width=220,height=40)
     
     def open_img(self):
         os.startfile("data")
 # ===================Function buttons===================
+    
+    def iExit(self):
+        self.iExit=tkinter.messagebox.askyesno("Face Recognition","Are you sure you want to exit this project ?",parent=self.root)
+        if self.iExit>0:
+            self.root.destroy()
+        else:
+            return    
+
 
     def student_details(self):
         self.new_window=Toplevel(self.root)
@@ -163,6 +188,20 @@ class Face_Recognition_System:
     def face_data(self):
         self.new_window=Toplevel(self.root)
         self.app=Face_Recognition(self.new_window)
+
+    def attendance_data(self):
+        self.new_window=Toplevel(self.root)
+        self.app=Attendance(self.new_window)
+
+    def developer_data(self):
+        self.new_window=Toplevel(self.root)
+        self.app=Developer(self.new_window)
+
+    def help_data(self):
+        self.new_window=Toplevel(self.root)
+        self.app=Help(self.new_window)   
+
+
     
 if __name__=="__main__":
     root=Tk()
